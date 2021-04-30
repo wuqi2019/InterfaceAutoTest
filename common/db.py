15f953e5dc729d0e8a16926953eba5faa8a2ab82
taoke <625531194@qq.com>
2021-04-30 17:02:54 +0800

    添加musql操作类

--- common/db.py
+++ common/db.py
@@ -1,4 +1,4 @@
-import redis
+import redis,pymysql
 
 # redis类
 class RedisBase:
@@ -15,6 +15,64 @@ class RedisString(RedisBase):
         """获取值"""
         result = self.r.get(xx)
         return result
+
+
+# mysql类
+class MYSQL:
+    """
+    对pymysql的简单封装
+    """
+    def __init__(self, host, port,user, pwd, db):
+        self.host = host
+        self.port = port
+        self.user = user
+        self.pwd = pwd
+        self.db = db
+
+    def __GetConnect(self):
+        """
+        得到连接信息
+        返回: conn.cursor()
+        """
+        self.conn = pymysql.connect(host=self.host, port=self.port, user=self.user,
+                                    password=self.pwd, database=self.db, charset="utf8")
+        cur = self.conn.cursor()
+        return cur
+
+    def ExecuQuery(self,sql):
+        """
+        执行查询语句
+        返回的是一个包含tuple的list，list的元素是记录行，tuple的元素是每行记录的字段
+
+        调用示例：
+                ms = MYSQL(host="localhost",user="sa",pwd="123456",db="PythonWeiboStatistics")
+                resList = ms.ExecQuery("SELECT id,NickName FROM WeiBoUser")
+                for (id,NickName) in resList:
+                    print str(id),NickName
+        """
+        cur = self.__GetConnect()
+        cur.execute(sql)
+        resList = cur.fetchall()        #查询所有,得到列表 [(1,2,3),(1,2,3),(1,2,3)]
+        # resList = cur.fetchone()      # 查询一个，得到1个(不用这个)
+
+        #查询完毕后必须关闭连接
+        self.conn.close()
+        return resList
+
+    def ExecuNonQuery(self,sql):
+        """
+        执行非查询语句
+
+        调用示例：
+            cur = self.__GetConnect()
+            cur.execute(sql)
+            self.conn.commit()
+            self.conn.close()
+        """
+        cur = self.__GetConnect()
+        cur.execute(sql)
+        self.conn.commit()
+        self.conn.close()
 
 
 
