0a1603cb3f0b147f79138201f9301a0eae9feaa5
fanxun <fanxun@hikcreate.com>
2021-04-30 13:58:20 +0800

    发送消息到钉钉群

--- common/utils.py
+++ common/utils.py
@@ -1,2 +1,15 @@
+import requests
+import json
+from config import BaseConfig
+
 class RedisBase:
     pass
+
+def dingTalk(webhook, message):
+    """发送消息到钉钉群"""
+    data = {'msgtype': 'text', 'text': {"content": message},
+            'at': {'isAtAll': True}}
+    post_data = json.dumps(data)
+    response = requests.post(webhook, headers=BaseConfig.headers, data=post_data)
+    return response.text
+
