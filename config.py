95104f7d3070b5a314b69ab4178bc7f095ac4ff9
fanxun <fanxun@hikcreate.com>
2021-04-30 13:46:37 +0800

    Merge branch 'master' of C:\Users\fanxun\Desktop\work\接口测试\接口自动化测试\data_A接口测试\InterfaceAutoTest with conflicts.

--- config.py
+++ config.py
@@ -1,10 +1,9 @@
-
 class BaseConfig():
     """基础配置类"""
     # 请求头
     headers = {'Content-Type': 'application/json; charset=utf-8'}
-    # 钉钉webhook
-    webhook = 'https://oapi.dingtalk.com/robot/send?access_token=b818bea4452f154e0dda34d89d1ce34c253dda51cb761ed9f78d07d3169fe1d2'
+    # 钉钉群webhook
+    webhook = ''
 
 
 
