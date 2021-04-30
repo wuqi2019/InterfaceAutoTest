5bb0a00e3f01c14874cb33ab94b0207162834993
taoke <625531194@qq.com>
2021-04-30 15:03:02 +0800

    造成冲突

--- common/utils.py
+++ common/utils.py
@@ -12,3 +12,7 @@ def dingTalk(webhook, message):
     post_data = json.dumps(data)
     response = requests.post(webhook, headers=BaseConfig.headers, data=post_data)
     return response.text
+qwew
+qwe
+qwe
+we
