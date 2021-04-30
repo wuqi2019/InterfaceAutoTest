268e348eeeef2ccea296d4920191ad421e291631
fanxun <fanxun@hikcreate.com>
2021-04-30 15:43:57 +0800

    解决冲突

--- common/utils.py
+++ common/utils.py
@@ -2,9 +2,6 @@ import requests
 import json
 from config import BaseConfig
 
-class RedisBase:
-    pass
-
 def dingTalk(webhook, message):
     """发送消息到钉钉群"""
     data = {'msgtype': 'text', 'text': {"content": message},
@@ -12,17 +9,3 @@ def dingTalk(webhook, message):
     post_data = json.dumps(data)
     response = requests.post(webhook, headers=BaseConfig.headers, data=post_data)
     return response.text
-
-
-
-
-
-
-qqqqqq
-aaaaaa
-aaaaaa
-21323213
-12321321
-1233213
-123321
-135123
