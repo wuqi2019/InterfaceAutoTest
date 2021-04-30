5f36f135c1b3512a94389b367566c3013a739f7f
huangchengcheng <1339094176@qq.com>
2021-04-30 17:18:02 +0800

    新增等待??函?

--- common/tools.py
+++
@@ -1,2 +0,0 @@
-def sheep():
-    pass
---
+++ common/utils/publicFunction.py
@@ -0,0 +1,14 @@
+import time
+
+def sleep(secs):
+    """等待secs秒"""
+    time.sleep(secs)
+
+
+
+
+if __name__ == "__main__":
+    pass
+    # print("start:%s"%time.ctime())
+    # sleep(5)
+    # print("end:%s"%time.ctime())
