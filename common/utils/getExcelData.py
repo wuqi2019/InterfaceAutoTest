269f589b005cd1b0d6601123384a5bd4020a5c23
taoke <625531194@qq.com>
2021-05-02 09:31:54 +0800

    调试读取excle方法

--- common/utils/getExcelData.py
+++ common/utils/getExcelData.py
@@ -1,18 +1,17 @@
 import xlrd,re,json
 
-
+"""调试中不可用"""
 workBook = xlrd.open_workbook('../data/营运车接口测试用例V1.0.xls')
 
 # 可以自动识别用例数
 def get_excelData(sheetName,caseName):
     """
     :param sheetName: sheet表名
-    :param caseName:  从excle中第一列编号 选择 要执行的编号（字母）
+    :param caseName:  从excle中第一列关键字（字母）
     :return: # [({参数},{期望}),({},{}),({},{})]
     """
 
     workSheet = workBook.sheet_by_name(sheetName)
-
 
     lis = []
     idx = 0
@@ -20,6 +19,8 @@ def get_excelData(sheetName,caseName):
         for one in workSheet.col_values(0):
             result = ''.join(re.findall(r'[A-Za-z]', one))  # 抽取字母字符串
             if caseName == result:
+                url= workSheet.cell_value(idx, 5)
+                headers= workSheet.cell_value(idx, 8)
                 colData = workSheet.cell_value(idx, 9)
                 colexpect = workSheet.cell_value(idx, 11)
                 lis.append((json.loads(colData), json.loads(colexpect)))  # 如果读取excle想获取字典，表中就必须是json字符串
