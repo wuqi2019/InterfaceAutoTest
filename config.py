267dc624a0df3fbd8c24885113b00755fe72875e
taoke <625531194@qq.com>
2021-05-06 18:40:02 +0800

    改变sso，的slat位置，让login.py不报错

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class BaseConfig():
     # 当前运行的产品名
     current_product = "bmc"
     default_test_case_dir = "test_case/bmc/"
-
+    salt = 'hikcreate_xj'                       # SSO的登录用的
 
 class BMCConfig(BaseConfig):
     """斑马信用app的配置类"""
@@ -16,5 +16,5 @@ class BMCConfig(BaseConfig):
     test_case_dir = "test_case/bmc/"
     test_case_data_dir = "test_case_data/bmc/"
     webhook = ''
-    salt = 'hikcreate_xj'
+
 
