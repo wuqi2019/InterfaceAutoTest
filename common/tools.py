338debc724b7d5bed6e2f7596e37cdd3dca380e0
jiaqiying <jiaqiying@hikcreate.com>
2021-05-09 23:48:37 -0700

    headers里content-type如果不是json的，模板里写明headers，不再做兼容主请求

--- common/tools.py
+++ common/tools.py
@@ -38,14 +38,6 @@ def request_main(url, headers, method, data):
         logging.log(str(e))
     except requests.exceptions.RequestException as e:
         logging.log(str(e))
-        if "mimeType" in headers.keys():
-            headers['mimeType'] = "application/x-www-form-urlencoded"
-        else:
-            headers['Content-Type'] = "application/x-www-form-urlencoded"
-        try:
-            res = request_by_method(method, headers)
-        except requests.exceptions.RequestException as e:
-            logging.log(str(e))
     if res != None:
         return res.json()
     return res
