f8fdf32c752a28b0f8736b2643734dea58dce0e8
jiaqiying <jiaqiying@hikcreate.com>
2021-05-09 20:27:30 -0700

    request_main 兼容不同的headers content_type和mimitype特殊字段

--- common/tools.py
+++ common/tools.py
@@ -9,23 +9,43 @@ from config import *
 def request_main(url, headers, method, data):
     """封装requests的通用请求方法"""
     res = None
+    def request_by_method(method, headers):
+        inner_res = None
+        try:
+            header_content_type = headers["Content-Type"]
+        except KeyError:
+            header_content_type = headers["mimeType"]
+
+        try:
+            if method.upper() == "GET":
+                inner_res = requests.get(url=url, headers=headers, params=data)
+            elif method.upper() == "POST":
+                if header_content_type == "application/json":
+                    inner_res = requests.post(url=url, headers=headers, data=json.dumps(data))
+                elif header_content_type in ["application/x-www-form-urlencoded"]:
+                    inner_res = requests.post(url=url, headers=headers, data=data)
+            return inner_res
+        except Exception as e:
+            logging.log(str(e))
+            raise Exception
+
     if headers == None or headers == {} or headers == "":
         # 如果传的headers为空，使用各自产品的通用headers
         headers = get_headers()
-    header_content_type = headers["Content-Type"]
-
     try:
-        if method.upper() == "GET":
-            res = requests.get(url=url, headers=headers, params=data)
-        elif method.upper() == "POST":
-            if header_content_type in ["application/x-www-form-urlencoded"]:
-                res = requests.post(url=url, headers=headers, data=data)
-            elif header_content_type == "application/json":
-                res = requests.post(url=url, headers=headers, data=json.dumps(data))
-    except Exception as e:
+        res = request_by_method(method, headers)
+    except requests.exceptions.ConnectionError as e:
         logging.log(str(e))
-        raise Exception
-
+    except requests.exceptions.RequestException as e:
+        logging.log(str(e))
+        if "mimeType" in headers.keys():
+            headers['mimeType'] = "application/x-www-form-urlencoded"
+        else:
+            headers['Content-Type'] = "application/x-www-form-urlencoded"
+        try:
+            res = request_by_method(method, headers)
+        except requests.exceptions.RequestException as e:
+            logging.log(str(e))
     if res != None:
         return res.json()
     return res
