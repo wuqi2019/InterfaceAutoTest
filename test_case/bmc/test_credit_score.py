ad6e1ae013b8db9414df5a98bdf17ec51d3a4959
taoke <625531194@qq.com>
2021-05-15 20:44:05 +0800

    屏蔽信用分和积分商城

--- test_case/bmc/test_credit_score.py
+++ test_case/bmc/test_credit_score.py
@@ -8,25 +8,25 @@ from common.tools import request_main
 from config import BMCConfig
 
 
-@allure.feature("信用分")
-class TestCreditScore():
-    workBook = xlrd.open_workbook(f'{BMCConfig.root_path}/test_case_data/bmc/bmc_testcase01_20210513.xlsx')
-
-    @allure.story("信用分")
-    @allure.title("{inData[testPoint]}")
-    @allure.testcase("{inData[yapiAddress]}")
-    @allure.description("url:/auth/login 。。。。")
-    @pytest.mark.parametrize("inData", get_excelData(workBook, '信用分', 'creditscore'))
-    def test_creditscore(self,inData):
-        url = f"{BMCConfig().host}{inData['url']}"
-        method  = inData['method']
-        req_data = inData['reqData']
-        expectData = inData['expectData']
-        headers = config.BMCConfig.headers
-        #res = requests.post(url = url,headers =headers,json =req_data )
-        res = request_main(url=url, headers=headers, method=method, data=req_data, has_token=True)
-        print(config.BMCConfig.headers)
-        assert res['code'] == expectData['code']
+# @allure.feature("信用分")
+# class TestCreditScore():
+#     workBook = xlrd.open_workbook(f'{BMCConfig.root_path}/test_case_data/bmc/bmc_testcase01_20210513.xlsx')
+#
+#     @allure.story("信用分")
+#     @allure.title("{inData[testPoint]}")
+#     @allure.testcase("{inData[yapiAddress]}")
+#     @allure.description("url:/auth/login 。。。。")
+#     @pytest.mark.parametrize("inData", get_excelData(workBook, '信用分', 'creditscore'))
+#     def test_creditscore(self,inData):
+#         url = f"{BMCConfig().host}{inData['url']}"
+#         method  = inData['method']
+#         req_data = inData['reqData']
+#         expectData = inData['expectData']
+#         headers = config.BMCConfig.headers
+#         #res = requests.post(url = url,headers =headers,json =req_data )
+#         res = request_main(url=url, headers=headers, method=method, data=req_data, has_token=True)
+#         print(config.BMCConfig.headers)
+#         assert res['code'] == expectData['code']
 
 if __name__ == '__main__':
     pytest.main(['-s', '-v', 'test_credit_score.py',
--- test_case/bmc/test_integral.py
+++ test_case/bmc/test_integral.py
@@ -8,23 +8,23 @@ from common.tools import request_main
 from config import BMCConfig
 
 
-@allure.feature("积分商城")
-class TestCreditScore():
-    workBook = xlrd.open_workbook(f'{BMCConfig.root_path}/test_case_data/bmc/bmc_testcase01_20210513.xlsx')
-
-    @allure.story("积分商城")
-    @allure.title("{inData[testPoint]}")
-    @pytest.mark.parametrize("inData", get_excelData(workBook, '积分商城', 'Integral'))
-    def test_creditscore(self,inData):
-        url = f"{BMCConfig().host}{inData['url']}"
-        method  = inData['method']
-        req_data = inData['reqData']
-        expectData = inData['expectData']
-        headers = config.BMCConfig.headers
-        #res = requests.post(url = url,headers =headers,json =req_data )
-        res = request_main(url=url, headers=headers, method=method, data=req_data, has_token=True)
-        print(res)
-        assert res['code'] == expectData['code']
+# @allure.feature("积分商城")
+# class TestCreditScore():
+#     workBook = xlrd.open_workbook(f'{BMCConfig.root_path}/test_case_data/bmc/bmc_testcase01_20210513.xlsx')
+#
+#     @allure.story("积分商城")
+#     @allure.title("{inData[testPoint]}")
+#     @pytest.mark.parametrize("inData", get_excelData(workBook, '积分商城', 'Integral'))
+#     def test_creditscore(self,inData):
+#         url = f"{BMCConfig().host}{inData['url']}"
+#         method  = inData['method']
+#         req_data = inData['reqData']
+#         expectData = inData['expectData']
+#         headers = config.BMCConfig.headers
+#         #res = requests.post(url = url,headers =headers,json =req_data )
+#         res = request_main(url=url, headers=headers, method=method, data=req_data, has_token=True)
+#         print(res)
+#         assert res['code'] == expectData['code']
 
 if __name__ == '__main__':
     pytest.main(['-s', '-v', 'test_integral.py',
