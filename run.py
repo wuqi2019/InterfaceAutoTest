02b6c86357833842e8a4c594c75dd05a9bc7c038
jiaqiying <jiaqiying@hikcreate.com>
2021-05-16 20:01:39 -0700

    修改删除以前报告的方式，注释掉调试用打印信息

--- run.py
+++ run.py
@@ -22,13 +22,12 @@ if __name__ == "__main__":
     print("********此次执行的产品测试用例是：%s********"%test_case_dir)
 
     #删除之前报告
-    for one in os.listdir('./report/tmp'):
-        if 'json' in one:
-            os.remove(f'./report/tmp/{one}')
+    os.system('rm ./report/tmp/*.json')
     # 生成报告数据
     pytest.main(['-v', '-s', test_case_dir, '--alluredir', './report/tmp'])
     # 打开报告
     # os.system('allure serve ./report/tmp')
+    # 发送钉钉
     dingTalk.dingTalk_markdown(secret="SEC1d08f46da74337cc0e1cd5bb9ad19622d825483343fdfa43ce396881e4745bdb",
                  webhook="https://oapi.dingtalk.com/robot/send?access_token=f9e005c1a984b9607960345d38669337b1115d1141a0294e98666443b312115b",
                                message="")
