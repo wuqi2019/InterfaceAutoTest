cf8eb6f9a5aef4de08ab247c91eb2823bf206692
fanxun <fanxun@hikcreate.com>
2021-05-17 11:44:48 +0800

    积分商城用例实现

--- test_case/bmc/test_credit_score.py
+++ test_case/bmc/test_credit_score.py
@@ -1,7 +1,9 @@
+from service.login import BMC
+
 __author__ = 'fanxun'
 __data__ = "2021-05-14 14:13"
 
-import pytest, allure, xlrd, os
+import pytest, allure, xlrd, os, requests
 import config
 from common.utils.getExcelData import  get_excelData
 from common.tools import request_main
@@ -9,30 +11,72 @@ from config import BMCConfig
 
 
 # @allure.feature("信用分")
-# class TestCreditScore():
-#     workBook = xlrd.open_workbook(f'{BMCConfig.root_path}/test_case_data/bmc/bmc_testcase01_20210513.xlsx')
-#
+class TestCreditScore():
+    workBook = xlrd.open_workbook(f'{BMCConfig.root_path}/test_case_data/bmc/bmc_testcase01_20210513.xlsx')
+    inData = get_excelData(workBook, '信用分', 'creditscore')[0]
+
+    def bmc_login_fixture(self):
+        """bmc登录获取token"""
+
+        indata = {"phone": "17822000000",
+                  "encodedGesture": "67e6d10010533eed4bbe9659863bf6ee"}
+        res = BMC().bmc_login(indata)
+        # setattr(BMCConfig, 'bmc_token', res[0])
+        # setattr(BMCConfig, 'bmc_pvt_token', res[1])
+        BMCConfig.headers['Pvt-Token'] = res[1]
+        BMCConfig.headers['Token'] = res[0]
+
+        print('Pvt-Token==========================', res[1])
+        print('Token==========================', res[0])
+
 #     @allure.story("信用分")
 #     @allure.title("{inData[testPoint]}")
 #     @allure.testcase("{inData[yapiAddress]}")
 #     @allure.description("url:/auth/login 。。。。")
 #     @pytest.mark.parametrize("inData", get_excelData(workBook, '信用分', 'creditscore'))
-#     def test_creditscore(self,inData):
-#         url = f"{BMCConfig().host}{inData['url']}"
-#         method  = inData['method']
-#         req_data = inData['reqData']
-#         expectData = inData['expectData']
-#         headers = config.BMCConfig.headers
-#         #res = requests.post(url = url,headers =headers,json =req_data )
-#         res = request_main(url=url, headers=headers, method=method, data=req_data, has_token=True)
-#         print(config.BMCConfig.headers)
-#         assert res['code'] == expectData['code']
+    def test_creditscore(self,inData):
+        url = f"{BMCConfig().host}{inData['url']}"
+        method  = inData['method']
+        req_data = inData['reqData']
+        expectData = inData['expectData']
+        headers = config.BMCConfig.headers
+        print(url)
+
+        #res = requests.post(url = url,headers =headers,json =req_data )
+        res = request_main(url=url, headers=headers, method=method, data=req_data, has_token=True)
+        print(res)
+        # assert res['code'] == expectData['code']
+
+
 
 if __name__ == '__main__':
-    pytest.main(['-s', '-v', 'test_credit_score.py',
-                 r'--alluredir=D:\项目\接口自动化\InterfaceAutoTest\report', '--clean-alluredir'])
-#
-    os.system('allure serve D:\项目\接口自动化\InterfaceAutoTest\\report')
+#     pytest.main(['-s', '-v', 'test_credit_score.py',
+#                  r'--alluredir=D:\项目\接口自动化\InterfaceAutoTest\report', '--clean-alluredir'])
+# #
+#     os.system('allure serve D:\项目\接口自动化\InterfaceAutoTest\\report')
+    workBook = xlrd.open_workbook(f'{BMCConfig.root_path}/test_case_data/bmc/bmc_testcase01_20210513.xlsx')
+    inData = get_excelData(workBook, '积分商城', 'Integral')[0]
+    TestCreditScore().bmc_login_fixture()
+    TestCreditScore().test_creditscore(inData)
 
+    # res = requests.get('http://testbmcapp.hikcreate.com/credit/myCredit/V2', params={"bCityCode":"520100","bNetTag":"trf_mgt"},
+    #                    headers= {
+    #     'City-Code': "520100",
+    #     'Device-Brand': "vivo",
+    #     'Device-Code': "000000001e167ed7000000001e167ed7",
+    #     'Device-Model': "vivo vivo X20",
+    #     'Device-Name': "vivo+X20",
+    #     'Device-Type': "Android",
+    #     'Mac': "38:6E:A2:A0:0E:AF",
+    #     'mimeType': "application/json",
+    #     'Net': "wifi",
+    #     'OS-Type': "Android",
+    #     'OS-Version': "27",
+    #     'Pvt-Token': "eyJhbGciOiJIUzI1NiIsInppcCI6IkRFRiJ9.eNpcjEEKgDAMBP-Ss0hS2kT7CP9Qa8CiVbB6Ev9uD56c0w4sc0MqBTzMaYmHhlPbuGdoIIUTPLEhQ2xZGijXWG_OICFRL4h1sRXnLTrHnccPUmLRSf5em8e-6hCy1k6YctrgeQEAAP__.GcCYQ7-NG3rSDLFUvgRVDS94QpYDBtisEuYSY4V_wAw",
+    #     'Resolution': "2034x1080",
+    #     'Token': "eTir/N9Z7ddMjuvo8M5MJWRLAWrlJ7pUlUe2+eYszHJumBknucBL6nuzBdYWFTWIpFiiDNjxV7Ehw32usHdd6VMFs0k7Rm70FcoDPkTEvyUkwhnN2GHlnk8nhxKvk3AJFDAj6JLl1Mr9OVj9I6TcjpQTzzyjzZjteIreMsUDwJW0Se+CW/teW+1DE70HDMY+0lrm01ftft627SGVsnr6AqxLF3KF/y+GyxKNrwKO29n8T33RFdWgcHs+fji46E/rBdSxxxJHjyClxnJOIoiU3DxZ/SoyNPn5X3CMGUio6MumAfJSQNh6Onk337G2tMum",
+    #     'Version': "2.2.6"
+    # })
+    # print(res.json())
 
 
--- test_case/bmc/test_integral.py
+++ test_case/bmc/test_integral.py
@@ -8,24 +8,248 @@ from common.tools import request_main
 from config import BMCConfig
 
 
-# @allure.feature("积分商城")
-# class TestCreditScore():
-#     workBook = xlrd.open_workbook(f'{BMCConfig.root_path}/test_case_data/bmc/bmc_testcase01_20210513.xlsx')
-#
-#     @allure.story("积分商城")
-#     @allure.title("{inData[testPoint]}")
-#     @pytest.mark.parametrize("inData", get_excelData(workBook, '积分商城', 'Integral'))
-#     def test_creditscore(self,inData):
-#         url = f"{BMCConfig().host}{inData['url']}"
-#         method  = inData['method']
-#         req_data = inData['reqData']
-#         expectData = inData['expectData']
-#         headers = config.BMCConfig.headers
-#         #res = requests.post(url = url,headers =headers,json =req_data )
-#         res = request_main(url=url, headers=headers, method=method, data=req_data, has_token=True)
-#         print(res)
-#         assert res['code'] == expectData['code']
+@allure.feature("积分商城")
+class TestCreditScore():
+    workBook = xlrd.open_workbook(f'{BMCConfig.root_path}/test_case_data/bmc/bmc_testcase01_20210513.xlsx')
 
+    @allure.story("查询我的积分")
+    @allure.link("http://yapi.hikcreate.com/project/31/interface/api/55860")
+    @allure.description("/integral/center/myIntegral")
+    @allure.title("{inData[testPoint]}")
+    @pytest.mark.parametrize("inData", get_excelData(workBook, '积分商城', 'myIntegral'))
+    def test_creditscore(self,inData):
+        url = f"{BMCConfig().host}{inData['url']}"
+        method  = inData['method']
+        req_data = inData['reqData']
+        expectData = inData['expectData']
+        headers = config.BMCConfig.headers
+        res = request_main(url=url, headers=headers, method=method, data=req_data, has_token=True)
+        assert res['code'] == expectData['code']
+
+    @allure.story("签到")
+    @allure.link("http://yapi.hikcreate.com/project/31/interface/api/55878")
+    @allure.description("/integral/center/sign")
+    @allure.title("{inData[testPoint]}")
+    @pytest.mark.parametrize("inData", get_excelData(workBook, '积分商城', 'postSignIntegral'))
+    def test_creditscore(self, inData):
+        url = f"{BMCConfig().host}{inData['url']}"
+        method = inData['method']
+        req_data = inData['reqData']
+        expectData = inData['expectData']
+        headers = config.BMCConfig.headers
+        res = request_main(url=url, headers=headers, method=method, data=req_data, has_token=True)
+        assert res['code'] == expectData['code']
+
+    @allure.story("查询我的签到情况")
+    @allure.link("http://yapi.hikcreate.com/project/31/interface/api/55869")
+    @allure.description("/integral/center/sign")
+    @allure.title("{inData[testPoint]}")
+    @pytest.mark.parametrize("inData", get_excelData(workBook, '积分商城', 'getsignIntegral'))
+    def test_creditscore(self, inData):
+        url = f"{BMCConfig().host}{inData['url']}"
+        method = inData['method']
+        req_data = inData['reqData']
+        expectData = inData['expectData']
+        headers = config.BMCConfig.headers
+        res = request_main(url=url, headers=headers, method=method, data=req_data, has_token=True)
+        assert res['code'] == expectData['code']
+
+    @allure.story("查询待领取积分清单")
+    @allure.link("http://yapi.hikcreate.com/project/31/interface/api/55887")
+    @allure.description("/integral/center/recommendedTasks")
+    @allure.title("{inData[testPoint]}")
+    @pytest.mark.parametrize("inData", get_excelData(workBook, '积分商城', 'taskIntegral'))
+    def test_creditscore(self, inData):
+        url = f"{BMCConfig().host}{inData['url']}"
+        method = inData['method']
+        req_data = inData['reqData']
+        expectData = inData['expectData']
+        headers = config.BMCConfig.headers
+        res = request_main(url=url, headers=headers, method=method, data=req_data, has_token=True)
+        assert res['code'] == expectData['code']
+
+    @allure.story("查询积分商品")
+    @allure.link("http://yapi.hikcreate.com/project/31/interface/api/57138")
+    @allure.description("/integral/center/integralGoods")
+    @allure.title("{inData[testPoint]}")
+    @pytest.mark.parametrize("inData", get_excelData(workBook, '积分商城', 'goodsIntegral'))
+    def test_creditscore(self, inData):
+        url = f"{BMCConfig().host}{inData['url']}"
+        method = inData['method']
+        req_data = inData['reqData']
+        expectData = inData['expectData']
+        headers = config.BMCConfig.headers
+        res = request_main(url=url, headers=headers, method=method, data=req_data, has_token=True)
+        assert res['code'] == expectData['code']
+
+    @allure.story("查询积分规则")
+    @allure.link("http://yapi.hikcreate.com/project/31/interface/api/64302")
+    @allure.description("/integral/center/rule")
+    @allure.title("{inData[testPoint]}")
+    @pytest.mark.parametrize("inData", get_excelData(workBook, '积分商城', 'ruleIntegral'))
+    def test_creditscore(self, inData):
+        url = f"{BMCConfig().host}{inData['url']}"
+        method = inData['method']
+        req_data = inData['reqData']
+        expectData = inData['expectData']
+        headers = config.BMCConfig.headers
+        res = request_main(url=url, headers=headers, method=method, data=req_data, has_token=True)
+        assert res['code'] == expectData['code']
+
+    @allure.story("设置签到提醒开关")
+    @allure.link("http://yapi.hikcreate.com/project/31/interface/api/56418")
+    @allure.description("/integral/center/sign/warnSwitch")
+    @allure.title("{inData[testPoint]}")
+    @pytest.mark.parametrize("inData", get_excelData(workBook, '积分商城', 'switchIntegral'))
+    def test_creditscore(self, inData):
+        url = f"{BMCConfig().host}{inData['url']}"
+        method = inData['method']
+        req_data = inData['reqData']
+        expectData = inData['expectData']
+        headers = config.BMCConfig.headers
+        res = request_main(url=url, headers=headers, method=method, data=req_data, has_token=True)
+        assert res['code'] == expectData['code']
+
+    @allure.story("任务中心主页")
+    @allure.link("http://yapi.hikcreate.com/project/31/interface/api/56427")
+    @allure.description("/integral/task/info")
+    @allure.title("{inData[testPoint]}")
+    @pytest.mark.parametrize("inData", get_excelData(workBook, '积分商城', 'infoIntegral'))
+    def test_creditscore(self, inData):
+        url = f"{BMCConfig().host}{inData['url']}"
+        method = inData['method']
+        req_data = inData['reqData']
+        expectData = inData['expectData']
+        headers = config.BMCConfig.headers
+        res = request_main(url=url, headers=headers, method=method, data=req_data, has_token=True)
+        assert res['code'] == expectData['code']
+
+    @allure.story("领取积分")
+    @allure.link("http://yapi.hikcreate.com/project/31/interface/api/56436")
+    @allure.description("/integral/task/receive")
+    @allure.title("{inData[testPoint]}")
+    @pytest.mark.parametrize("inData", get_excelData(workBook, '积分商城', 'receiveIntegral'))
+    def test_creditscore(self, inData):
+        url = f"{BMCConfig().host}{inData['url']}"
+        method = inData['method']
+        req_data = inData['reqData']
+        expectData = inData['expectData']
+        headers = config.BMCConfig.headers
+        res = request_main(url=url, headers=headers, method=method, data=req_data, has_token=True)
+        assert res['code'] == expectData['code']
+
+    @allure.story("获取关注信息")
+    @allure.link("http://yapi.hikcreate.com/project/31/interface/api/56841")
+    @allure.description("/integral/task/getFollowInfo")
+    @allure.title("{inData[testPoint]}")
+    @pytest.mark.parametrize("inData", get_excelData(workBook, '积分商城', 'FollowInfoIntegral'))
+    def test_creditscore(self, inData):
+        url = f"{BMCConfig().host}{inData['url']}"
+        method = inData['method']
+        req_data = inData['reqData']
+        expectData = inData['expectData']
+        headers = config.BMCConfig.headers
+        res = request_main(url=url, headers=headers, method=method, data=req_data, has_token=True)
+        assert res['code'] == expectData['code']
+
+
+    @allure.story("商城中的商品")
+    @allure.link("http://yapi.hikcreate.com/project/31/interface/api/55896")
+    @allure.description("/integral/mall/goods/list")
+    @allure.title("{inData[testPoint]}")
+    @pytest.mark.parametrize("inData", get_excelData(workBook, '积分商城', 'listIntegral'))
+    def test_creditscore(self, inData):
+        url = f"{BMCConfig().host}{inData['url']}"
+        method = inData['method']
+        req_data = inData['reqData']
+        expectData = inData['expectData']
+        headers = config.BMCConfig.headers
+        res = request_main(url=url, headers=headers, method=method, data=req_data, has_token=True)
+        assert res['code'] == expectData['code']
+
+    @allure.story("商品的详情")
+    @allure.link("http://yapi.hikcreate.com/project/31/interface/api/56076")
+    @allure.description("/integral/mall/goods/detail")
+    @allure.title("{inData[testPoint]}")
+    @pytest.mark.parametrize("inData", get_excelData(workBook, '积分商城', 'detailIntegral'))
+    def test_creditscore(self, inData):
+        url = f"{BMCConfig().host}{inData['url']}"
+        method = inData['method']
+        req_data = inData['reqData']
+        expectData = inData['expectData']
+        headers = config.BMCConfig.headers
+        res = request_main(url=url, headers=headers, method=method, data=req_data, has_token=True)
+        assert res['code'] == expectData['code']
+
+    @allure.story("兑换商品接口")
+    @allure.link("http://yapi.hikcreate.com/project/31/interface/api/56085")
+    @allure.description("/integral/mall/goods/exchange")
+    @allure.title("{inData[testPoint]}")
+    @pytest.mark.parametrize("inData", get_excelData(workBook, '积分商城', 'exchangeIntegral'))
+    def test_creditscore(self, inData):
+        url = f"{BMCConfig().host}{inData['url']}"
+        method = inData['method']
+        req_data = inData['reqData']
+        expectData = inData['expectData']
+        headers = config.BMCConfig.headers
+        res = request_main(url=url, headers=headers, method=method, data=req_data, has_token=True)
+        assert res['code'] == expectData['code']
+
+    @allure.story("查询商品的适用门店列表")
+    @allure.link("http://yapi.hikcreate.com/project/31/interface/api/56094")
+    @allure.description("/integral/mall/goods/applicableStores")
+    @allure.title("{inData[testPoint]}")
+    @pytest.mark.parametrize("inData", get_excelData(workBook, '积分商城', 'applicableStoresIntegral'))
+    def test_creditscore(self, inData):
+        url = f"{BMCConfig().host}{inData['url']}"
+        method = inData['method']
+        req_data = inData['reqData']
+        expectData = inData['expectData']
+        headers = config.BMCConfig.headers
+        res = request_main(url=url, headers=headers, method=method, data=req_data, has_token=True)
+        assert res['code'] == expectData['code']
+
+    @allure.story("查询兑换记录")
+    @allure.link("http://yapi.hikcreate.com/project/31/interface/api/56103")
+    @allure.description("/integral/mall/exchangeRecord/list")
+    @allure.title("{inData[testPoint]}")
+    @pytest.mark.parametrize("inData", get_excelData(workBook, '积分商城', 'exchangeRecordListIntegral'))
+    def test_creditscore(self, inData):
+        url = f"{BMCConfig().host}{inData['url']}"
+        method = inData['method']
+        req_data = inData['reqData']
+        expectData = inData['expectData']
+        headers = config.BMCConfig.headers
+        res = request_main(url=url, headers=headers, method=method, data=req_data, has_token=True)
+        assert res['code'] == expectData['code']
+
+    @allure.story("查询兑换记录详情")
+    @allure.link("http://yapi.hikcreate.com/project/31/interface/api/57354")
+    @allure.description("/integral/mall/exchangeRecord/detail")
+    @allure.title("{inData[testPoint]}")
+    @pytest.mark.parametrize("inData", get_excelData(workBook, '积分商城', 'exchangeRecordDetailIntegral'))
+    def test_creditscore(self, inData):
+        url = f"{BMCConfig().host}{inData['url']}"
+        method = inData['method']
+        req_data = inData['reqData']
+        expectData = inData['expectData']
+        headers = config.BMCConfig.headers
+        res = request_main(url=url, headers=headers, method=method, data=req_data, has_token=True)
+        assert res['code'] == expectData['code']
+
+    @allure.story("关注成功回调")
+    @allure.link("http://yapi.hikcreate.com/project/31/interface/api/61233")
+    @allure.description("/integral/task/focusSuccess")
+    @allure.title("{inData[testPoint]}")
+    @pytest.mark.parametrize("inData", get_excelData(workBook, '积分商城', 'focusSuccessIntegral'))
+    def test_creditscore(self, inData):
+        url = f"{BMCConfig().host}{inData['url']}"
+        method = inData['method']
+        req_data = inData['reqData']
+        expectData = inData['expectData']
+        headers = config.BMCConfig.headers
+        res = request_main(url=url, headers=headers, method=method, data=req_data, has_token=True)
+        assert res['code'] == expectData['code']
 if __name__ == '__main__':
     pytest.main(['-s', '-v', 'test_integral.py',
                  r'--alluredir=D:\项目\接口自动化\InterfaceAutoTest\report', '--clean-alluredir'])
