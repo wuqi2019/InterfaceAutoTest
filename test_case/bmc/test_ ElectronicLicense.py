fb8ec8d7d9b700056818dbe05fecd3cd07533043
taoke <625531194@qq.com>
2021-05-18 14:11:19 +0800

    excle电子证照修复数据

--- test_case/bmc/test_ ElectronicLicense.py
+++ test_case/bmc/test_ ElectronicLicense.py
@@ -36,6 +36,8 @@ def get_vehickleId_Tounbind():
 
 
 
+
+
 @allure.epic("电子证照")
 # @allure.feature("电子驾驶证")
 class TestDrivingLicense():
@@ -44,7 +46,7 @@ class TestDrivingLicense():
     @allure.link("http://yapi.hikcreate.com/project/32/interface/api/81596")
     @allure.description("/dlVeh/qr")
     @allure.title("{inData[testPoint]}")
-    @pytest.mark.parametrize("inData", get_excelData(workBook,'电子证照', 'dlVehqr'))
+    @pytest.mark.parametrize("inData", get_excelData(workBook, '电子证照', 'dlVehqr'))
     def test_dlVehQr(self,inData):
         url = f"{BMCConfig().pvthost}{inData['url']}"
         method = inData['method']
@@ -71,6 +73,7 @@ class TestDrivingLicense():
         res = request_main(url, headers, method, req_data)
         """断言"""
         assert res['code'] == expectData['code']
+
 
     @allure.story("照片审核状态")
     @allure.link("http://yapi.hikcreate.com/project/32/interface/api/22759")
@@ -104,6 +107,7 @@ class TestDrivingLicense():
         """断言"""
         assert res['code'] == expectData['code']
 
+    @pytest.mark.scoreDetail
     @allure.story("修改驾驶证头像")
     @allure.link("http://yapi.hikcreate.com/project/32/interface/api/22750")
     @allure.description("/drivingLicense/avatar/update")
@@ -120,7 +124,7 @@ class TestDrivingLicense():
         """断言"""
         assert res['code'] == expectData['code']
 
-    # @pytest.mark.scoreDetail
+
     @allure.story("驾照扣分记录")
     @allure.link("http://yapi.hikcreate.com/project/32/interface/api/5332")
     @allure.description("/drivingLicense/score/detail")
@@ -154,8 +158,6 @@ class TestDrivingLicense():
         # assert res['code'] == expectData['code']
 
 
-    # @pytest.fixture(scope='function')       # 用例中不能这么写
-    # @pytest.mark.usefixtures('test_vehicleLicenseList')
     @allure.story("我的行驶证列表")
     @allure.link("http://yapi.hikcreate.com/project/32/interface/api/46536")
     @allure.description("/vehicle/license/list")
@@ -373,7 +375,7 @@ if __name__ == '__main__':
     for one in os.listdir('../../report/tmp'):  # 列出对应文件夹的数据  '-m','scoreDetail' ,
         if 'json' in one:
             os.remove(f'../../report/tmp/{one}')
-    pytest.main(['test_ ElectronicLicense.py', '-s',     '--alluredir','../../report/tmp'])
+    pytest.main(['test_ ElectronicLicense.py', '-s',  '--alluredir','../../report/tmp'])
     # 启动默认浏览器打开报告
     os.system('allure serve ../../report/tmp')
 
