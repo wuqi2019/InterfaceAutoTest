de66a79762019cc1f625bf574c3fcac86c297d29
fanxun <fanxun@hikcreate.com>
2021-05-25 14:11:28 +0800

    更改注册用例数据

--- test_case/bmc/test_accountinfo.py
+++ test_case/bmc/test_accountinfo.py
@@ -12,39 +12,44 @@ from common.tools import request_main
 from config import BMCConfig
 
 
-@allure.feature("账号信息基本功能")
-class TestLogin():
-    workBook = xlrd.open_workbook(f'{BMCConfig.root_path}/test_case_data/bmc/bmc_login_20210513.xlsx')
-    @allure.story("登录")
-    @allure.severity("")
-    @allure.title("{inData[testPoint]}")
-    @allure.testcase("{inData[yapiAddress]}")
-    @allure.description("url:/auth/login 。。。。")
-    @pytest.mark.parametrize("inData", get_excelData(workBook,'登录', 'login'))
-    def test_login(self,inData):
-        url = f"{BMCConfig().host}{inData['url']}"
-        method  = inData['method']
-        req_data = inData['reqData']
-        expectData = inData['expectData']
-        headers = config.BMCConfig.loginheader
-        res = request_main(url= url,headers = headers,method =method,data = req_data,has_token=True)
-        print(res)
-        assert res['code'] == expectData['code']
+# @allure.feature("账号信息基本功能")
+# class TestLogin():
+#     workBook = xlrd.open_workbook(f'{BMCConfig.root_path}/test_case_data/bmc/bmc_login_20210513.xlsx')
+#     @allure.story("登录")
+#     @allure.severity("")
+#     @allure.title("{inData[testPoint]}")
+#     @allure.testcase("{inData[yapiAddress]}")
+#     @allure.description("url:/auth/login 。。。。")
+#     @pytest.mark.parametrize("inData", get_excelData(workBook,'登录', 'login'))
+#     def test_login(self,inData):
+#         url = f"{BMCConfig().host}{inData['url']}"
+#         method  = inData['method']
+#         req_data = inData['reqData']
+#         expectData = inData['expectData']
+#         headers = config.BMCConfig.loginheader
+#         res = request_main(url= url,headers = headers,method =method,data = req_data,has_token=True)
+#         assert res['code'] == expectData['code']
+#
+#     @allure.story("激活")
+#     @allure.link("")
+#     @allure.description("/user/credit/idAuth")
+#     @allure.title("{inData[testPoint]}")
+#     @pytest.mark.parametrize("inData", get_excelData(workBook, '登录', 'Active'))
+#     def test_active(self,inData):
+#         url = f"{BMCConfig().host}{inData['url']}"
+#         method = inData['method']
+#         req_data = inData['reqData']
+#         expectData = inData['expectData']
+#         headers = config.BMCConfig.headers
+#         res = request_main(url=url, headers=headers, method=method, data=req_data)
+#         assert res['code'] == expectData['code']
 
-    @allure.story("激活")
-    @allure.link("")
-    @allure.description("/user/credit/idAuth")
-    @allure.title("{inData[testPoint]}")
-    @pytest.mark.parametrize("inData", get_excelData(workBook, '登录', 'Active'))
-    def test_active(self,inData):
-        url = f"{BMCConfig().host}{inData['url']}"
-        method = inData['method']
-        req_data = inData['reqData']
-        expectData = inData['expectData']
-        headers = config.BMCConfig.headers
-        res = request_main(url=url, headers=headers, method=method, data=req_data)
-        print(res)
-        assert res['code'] == expectData['code']
+
+class TestRegister():
+    """注册"""
+    workBook = xlrd.open_workbook(f'{BMCConfig.root_path}/test_case_data/bmc/bmc_base_info_2021513.xlsx')
+
+
 
 if __name__ == '__main__':
     pytest.main(['-s', '-v', 'test_accountinfo.py',
