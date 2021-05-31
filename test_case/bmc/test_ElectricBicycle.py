4d15f544e17ce38aaed1b631f8d53985e992ab0b
taoke <625531194@qq.com>
2021-05-31 17:00:18 +0800

    电动车添加获取“通行证类型”的状态 用例

---
+++ test_case/bmc/test_ElectricBicycle.py
@@ -0,0 +1,43 @@
+#作者: taoke
+#时间: 2021/5/31 16:34
+#编码: -- coding: utf-8 --
+#版本: !python3.7
+
+import pytest,allure,xlrd,requests,os
+from common.utils.getExcelData import  get_excelData
+from service.login import BMY
+from common.tools import request_main
+from config import BaseConfig,BMCConfig
+from service.login import BMC
+from common.db import RedisString,MYSQL
+
+# @allure.epic("斑马信用")
+@allure.feature("电动车")
+class TestDrivingLicense():
+    workBook = xlrd.open_workbook(f'{BaseConfig.root_path}/test_case_data/bmc/bmc_ElectricBicycle_20210531.xlsx')
+    def setup_class(self):
+        pass
+
+    @allure.story("电动自行车预约")
+    @allure.link("http://yapi.hikcreate.com/project/32/interface/api/19711")
+    @allure.description("/pvtapi/deliveryVehicle/passcardTypeState")
+    @allure.title("{inData[testPoint]}")
+    @pytest.mark.parametrize("inData", get_excelData(workBook, '电动车', 'passcardTypeState'))
+    def test_passcardTypeState(self,inData):
+        url = f"{BMCConfig().host}{inData['url']}"
+        method = inData['method']
+        req_data = inData['reqData']
+        expectData = inData['expectData']
+        headers = inData['headers']
+        """请求"""
+        res = request_main(url, headers, method, req_data)
+        allure.attach(f"{res}", "响应结果", allure.attachment_type.TEXT)
+        assert res['code'] == expectData['code']
+
+
+if __name__ == '__main__':
+    for one in os.listdir('../../report/tmp'):  #  '-m','scoreDetail' ,
+        if 'json' in one:
+            os.remove(f'../../report/tmp/{one}')
+    pytest.main(['test_ElectricBicycle.py', '-s',  '--alluredir','../../report/tmp'])
+    os.system('allure serve ../../report/tmp')
