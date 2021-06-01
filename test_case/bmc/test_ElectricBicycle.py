23a0eb2bf36f5162ecdae9d964b3131f8b22f2d2
taoke <625531194@qq.com>
2021-06-01 16:29:18 +0800

    电动车添加用例

--- test_case/bmc/test_ElectricBicycle.py
+++ test_case/bmc/test_ElectricBicycle.py
@@ -102,7 +102,7 @@ class TestDrivingLicense():
         allure.attach(f"{res}", "响应结果", allure.attachment_type.TEXT)
         assert res['code'] == expectData['code']
 
-    # @pytest.mark.scoreDetail
+
     @allure.story("获取须知信息")
     @allure.link("http://yapi.hikcreate.com/project/31/interface/api/7704")
     @allure.description("接口：/pvtapi/sys/notice/detail，creator：胥键雪，autoCreator：taoke")
@@ -110,6 +110,60 @@ class TestDrivingLicense():
     @pytest.mark.parametrize("inData", get_excelData(workBook, '电动车', 'noticeDetail'))
     def test_noticeDetail(self, inData):
         url = f"{BMCConfig().host}{inData['url']}"
+        method = inData['method']
+        req_data = inData['reqData']
+        expectData = inData['expectData']
+        headers = inData['headers']
+        """请求"""
+        res = request_main(url, headers, method, req_data)
+        allure.attach(f"{res}", "响应结果", allure.attachment_type.TEXT)
+        assert res['code'] == expectData['code']
+
+
+    @allure.story("文件上传")
+    @allure.link("http://yapi.hikcreate.com/project/32/interface/api/5452")
+    @allure.description("接口：/pvtapi/file/upload，creator：胥键雪，autoCreator：taoke")
+    @allure.title("{inData[testPoint]}")
+    @pytest.mark.parametrize("inData", get_excelData(workBook, '电动车', 'fileUpload'))
+    def test_fileUpload(self, inData):
+        url = f"{BMCConfig().host}{inData['url']}"
+        expectData = inData['expectData']
+        """特殊处理"""
+        headers = {'City-Code': "520100", 'Device-Brand': "vivo", 'Device-Code': "000000001e167ed7000000001e167ed7",
+                   'Device-Model': "vivo vivo X20", 'Device-Name': "vivo+X20", 'Device-Type': "Android",
+                   'Mac': "38:6E:A2:A0:0E:AF", 'mimeType': "application/json", 'Net': "wifi", 'OS-Type': "Android",
+                   'OS-Version': "27", 'Pvt-Token': f"{BMCConfig.bmc_pvt_token}", 'Resolution': "2034x1080",
+                   'Token': f"{BMCConfig.bmc_token}", 'Version': "2.2.6"}
+        user_file = {'file': ("321.gif", open(f"{BaseConfig.root_path}/test_case_data/bmc/321.gif", 'rb'), 'image/png/jpg')}
+        res = requests.post(url, files=user_file, headers=headers)
+        allure.attach(f"{res.json()}", "响应结果", allure.attachment_type.TEXT)
+        assert res.json()['code'] == expectData['code']
+
+
+    @allure.story("获取系统字典")
+    @allure.link("http://yapi.hikcreate.com/project/31/interface/api/5077")
+    @allure.description("接口：/pvtapi/sys/dict/list，creator：胥键雪，autoCreator：taoke")
+    @allure.title("{inData[testPoint]}")
+    @pytest.mark.parametrize("inData", get_excelData(workBook, '电动车', 'sysDictlist'))
+    def test_sysDictlist(self, inData):
+        url = f"{BMCConfig().host}{inData['url']}"
+        method = inData['method']
+        req_data = inData['reqData']
+        expectData = inData['expectData']
+        headers = inData['headers']
+        """请求"""
+        res = request_main(url, headers, method, req_data)
+        allure.attach(f"{res}", "响应结果", allure.attachment_type.TEXT)
+        assert res['code'] == expectData['code']
+
+    @pytest.mark.scoreDetail
+    @allure.story("地址选择")
+    @allure.link("http://yapi.hikcreate.com/project/32/interface/api/45572")
+    @allure.description("接口：/pvtapi/sys/dict/list，creator：胥键雪，autoCreator：taoke")
+    @allure.title("{inData[testPoint]}")
+    @pytest.mark.parametrize("inData", get_excelData(workBook, '电动车', 'addressSelector'))
+    def test_addressSelector(self, inData):
+        url = f"{BMCConfig().pvthost}{inData['url']}"
         method = inData['method']
         req_data = inData['reqData']
         expectData = inData['expectData']
